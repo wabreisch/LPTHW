ed38a9c2cd55388ca932ed33499d8c9eab09839e
Wes Breisch <wabreisch@quinnipiac.edu>
2015-04-07 13:16:17 -0400

    Pushing ex02 through ex05

---
+++ ex05/ex05.py
@@ -0,0 +1,17 @@
+name = 'Wesley A. Breisch'
+age = 21
+height = 73 #inches
+weight = 178 #pounds
+eyes = 'Green'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
