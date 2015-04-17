94dae47bdfed8165d3f08bfc3cb25b113976f886
Wes Breisch <wabreisch@quinnipiac.edu>
2015-04-16 20:58:16 -0400

    Skipping ahead to ex39 to practice dicts o.o

---
+++ ex39/ex39.py
@@ -0,0 +1,18 @@
+# stuff = {'name': 'Zed', 'age': 39, 'height': 6*12 + 2}
+# print stuff['name']
+# print stuff['age']
+# print stuff['height']
+# stuff['city'] = 'San Francisco'
+# print stuff['city']
+
+# stuff[1] = "Wow"
+# stuff[2] = "Neato"
+# print stuff[1]
+# print stuff[2]
+# print stuff
+
+# del stuff['city']
+# del stuff[1]
+# del stuff[2]
+# print stuff
+
